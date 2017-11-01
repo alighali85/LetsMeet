80a5bed313ea38cea0e35296b67452886d9176d4
Ali <ali@alighali.com>
2017-11-01 01:31:26 +0100

    add header footer and body

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, LoginComponent, HeaderComponent, BodyComponent, FooterComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
