ace271793f699ba810def343fb47b0f66834d803
Ali <ali@alighali.com>
2017-12-17 23:35:30 +0100

    add code

--- newLetsMeet/ng5/src/app/home/home.component.ts
+++ newLetsMeet/ng5/src/app/home/home.component.ts
@@ -1,24 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {trigger, transition, query, animate,keyframes, style, stagger} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  /*animations : [
-    trigger('goals',[
-      transition ('* => *', [
-        query(':enter', style({ opacity:0 }), {optional: true}),
-
-        query( ':enter', stagger('300ms', [
-          animate('.6s ease-in', keyframes([
-            style({opacity: 0, transform: 'translateY(-75%)',offset: 0}),
-            style({opacity: .5, transform: 'translateY(35px)',offset: 3}),
-            style({opacity: 1, transform: 'translateY(0)',offset: 1}),]))
-
-        ]), {optional: true})
-      ])
-    ])
-  ]*/
 })
 export class HomeComponent implements OnInit {
   itemCount: number;
@@ -35,6 +21,10 @@ export class HomeComponent implements OnInit {
   addItem(){
     this.goals.push(this.goalText);
     this.itemCount = this.goals.length;
+  }
+
+  removeItem(i) {
+    this.goals.splice(i , 1);
   }
 
 }
