7031c53e6aebe6addf633401f62f78d515b76fec
Ali <ali@alighali.com>
2018-02-04 23:51:42 +0100

    add firebase config to projuct

--- newLetsMeet/ng5/src/environments/environment.ts
+++ newLetsMeet/ng5/src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBr3ZD_dHrN9TIcC7TzF423A2TX8hEwcIw",
+    authDomain: "livedate-46649.firebaseapp.com",
+    databaseURL: "https://livedate-46649.firebaseio.com",
+    projectId: "livedate-46649",
+    storageBucket: "livedate-46649.appspot.com",
+    messagingSenderId: "557029138050"
+  }
 };
